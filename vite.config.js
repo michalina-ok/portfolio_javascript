689fdab9a1c598ed98156e4d86c030b786a96906
Michalina <michalina.oniszczuk@gmail.com>
2022-11-22 14:13:08 +0100

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 // vite.config.js
-const { resolve } = require("path");
- 
-module.exports = {
+import { resolve } from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
+  base: "./",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
-        contact: resolve(__dirname, "contact.html"),
-        projects: resolve(__dirname, "projects.html"),
       },
     },
   },
-};
+});
 
